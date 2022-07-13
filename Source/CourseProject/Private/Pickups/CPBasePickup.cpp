544b42c3fc4b00ed46315c1936daa54084216ac0
f.a.n.163@mail.ru <f.a.n.163@mail.ru>
2022-07-13 17:47:48 +0400

    Добавлено вращение Pickup

--- Source/CourseProject/Private/Pickups/CPBasePickup.cpp
+++ Source/CourseProject/Private/Pickups/CPBasePickup.cpp
@@ -21,11 +21,15 @@ void ACPBasePickup::BeginPlay()
     Super::BeginPlay();
 
     check(CollisionComponent);
+
+    GenerateRotationYaq();
 }
 
 void ACPBasePickup::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
+
+    AddActorLocalRotation(FRotator(0.0f, RotationYaw, 0.0f));
 }
 
 void ACPBasePickup::NotifyActorBeginOverlap(AActor* OtherActor)
@@ -58,9 +62,17 @@ void ACPBasePickup::PickupWasTaken()
 
 void ACPBasePickup::Respawn()
 {
+    GenerateRotationYaq();
+
     CollisionComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
     if (GetRootComponent())
     {
         GetRootComponent()->SetVisibility(true, true);
     }
+}
+
+void ACPBasePickup::GenerateRotationYaq()
+{
+    const auto Direction = FMath::RandBool() ? 1.0f : -1.0f;
+    RotationYaw = FMath::RandRange(1.0f, 2.0f) * Direction;
 }
